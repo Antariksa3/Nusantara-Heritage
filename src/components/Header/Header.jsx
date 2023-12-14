80bffdbe45b4a3cae6467ba2de6c39ee6daf95c4
mumtazalwan <mumtazalwan@gmail.com>
2023-12-14 13:39:27 +0700

    style: navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
     return (
         <header>
-            <nav>ini navbar</nav>
+            <p className='web-name'>NUSANTARA HERITAGE</p>
+            <nav>
+                <a href="">Provinsi</a>
+                <a href="">Wisata</a>
+                <a href="">Kuliner</a>
+                <a href="">Kebudayaan</a>
+                <a href="">Bahasa</a>
+            </nav>
         </header>
     )
 }
