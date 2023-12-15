3d9464a268e245c2d23c3e5185ca5bd912e783db
mumtazalwan <mumtazalwan@gmail.com>
2023-12-15 23:19:22 +0700

    style: responsive navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,29 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
 
-    const [showNavbar, setShowNavbar] = useState(false)
+    const [showSidebar, setShowSidebar] = useState(false)
+    const [addBackground, setAddBackground] = useState(false)
 
-    const handleShowNavbar = () => {
-        setShowNavbar(!showNavbar)
+    const handleShowSidebar = () => {
+        setShowSidebar(!showSidebar)
     }
 
+    const changeBackground = () => {
+        if (window.scrollY >= 80) {
+            setAddBackground(true);
+        } else {
+            setAddBackground(false);
+        }
+    }
+
+    window.addEventListener('scroll', changeBackground);
+
     return (
-        <header>
+        <header className={`header ${addBackground && 'active'}`}>
             <p className='web-name'>NUSANTARA HERITAGE</p>
-            <div className={`menu ${showNavbar && 'active'}`} onClick={handleShowNavbar}>=</div>
-            <nav className={`nav-a ${showNavbar && 'active'}`}>
-                <div className={`close-btn ${showNavbar && 'active'}`} onClick={handleShowNavbar}>x</div>
+            <div className={`menu ${showSidebar && 'active'}`} onClick={handleShowSidebar}>=</div>
+            <nav className={`nav-a ${showSidebar && 'active'}`}>
+                <div className={`close-btn ${showSidebar && 'active'}`} onClick={handleShowSidebar}>x</div>
                 <NavLink to={""}>Provinsi</NavLink>
                 <NavLink to={""}>Wisata</NavLink>
                 <NavLink to={""}>Kuliner</NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                 <Header />
             </BrowserRouter>
             <Main />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 }
