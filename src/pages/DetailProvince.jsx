2cbd57417929b22d112751a291ba197d14d55c11
Antariksa3 <antariksakusumaharmawan6801@gmail.com>
2023-12-17 23:34:34 +0700

    style: discover carousel

--- src/pages/DetailProvince.jsx
+++ src/pages/DetailProvince.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import DetailExplore from '../components/DetailExplore/DetailExplore'
 import Footer from '../components/Footer/Footer'
-import { BrowserRouter } from 'react-router-dom'
 
 const DetailProvince = () => {
     return (
         <div>
-            <BrowserRouter>
-                <Header />
-            </BrowserRouter>
+            <Header />
             <DetailExplore />
             <Footer />
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import Main from '../components/Main/Main'
 import Footer from '../components/Footer/Footer'
-import { BrowserRouter } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
-            <BrowserRouter>
-                <Header />
-            </BrowserRouter>
+            <Header />
             <Main />
             <Footer />
         </div>
